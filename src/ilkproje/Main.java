ba4ab0c6fd93ec1cbffec11588736bd487a63b0a
beytullahsari14 <beytullahsari14@yandex.com>
2021-11-09 16:27:00 +0300

    Initial commit

---
+++ src/ilkproje/Main.java
@@ -0,0 +1,18 @@
+package ilkproje;
+
+import com.sun.tools.classfile.StackMapTable_attribute.same_frame;
+
+public class Main {
+
+	public static void main(String[] args) 
+		
+	
+		// TODO Auto-generated method stub
+        // sysout ctrl space 
+
+		
+		
+		  
+		
+		
+		
